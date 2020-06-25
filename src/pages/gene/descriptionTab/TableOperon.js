f95d8624bb531a40880fdeee141f456a6b21f858
Gabriel Alarcon <34384736+Galarcon128@users.noreply.github.com>
2020-06-25 16:09:43 -0500

    update TableOperon

--- src/pages/gene/descriptionTab/TableOperon.js
+++ src/pages/gene/descriptionTab/TableOperon.js
@@ -1,38 +1,75 @@
 import React from 'react';
-import {RegulationInfo} from '../../components/apollo/geneCollection'
+import { RegulationInfo } from '../../components/apollo/geneCollection'
+import { useHistory } from 'react-router-dom';
 
 const TableOperon = ({
     idGene
 }) => {
     let gene = new RegulationInfo(idGene)
-    const {loading,data,error} = gene
-    console.log("info: ",idGene)
-    console.log("data: ",data)
+    const { loading, data, error } = gene
+    let history = useHistory();
+    // console.log("info: ",idGene)
+
     // console.log("error: ",error)
 
-    if(loading){
+    if (loading) {
         return <>loading...</>
-    }else{
-        if(error !== undefined){
+    } else {
+        if (error !== undefined) {
             return <>error</>
-        }else{
+        } else {
             try {
-                let name = data.name
-                let id = data.id
-                return(
+                const name = data.name
+                const id = data.id
+                //console.log("data: ", data.arrangement)
+                const arrangement = data.arrangement
+                return (
                     <div style={{ width: "80%" }}>
-                       <h2 style={{color: "var(--color-accentB)", margin: "0", float: "left"}}>
-                           Operon &nbsp;
+                        <h2 style={{ color: "var(--color-accentB)", margin: "0", float: "left" }}>
+                            Operon &nbsp;
                        </h2>
-                    <h2 style={{margin: "0"}}>{name}</h2>
-                    <h3 style={{margin: "0", fontSize: "9px"}}>{id}</h3>
+                        <h2 style={{ margin: "0" }}>{name}</h2>
+                        <h3 style={{ margin: "0", fontSize: "9px" }}>{id}</h3>
+                        <h3>arrangement:</h3>
+                        <table >
+                            <thead>
+                                <tr>
+                                    <th>Promoter</th>
+                                    <th>Regulator</th>
+                                    <th>TranscriptionUnit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    arrangement.map((item) => {
+                                        //console.log("item: ", item)
+                                        const promoter = item.promoter
+                                        const regulator = item.regulator
+                                        const tu = item.transcriptionUnit
+                                        return (
+                                            <tr key={promoter.id}  >
+                                                <td className="trClickable" onClick={() => { history.push("/promoter/" + promoter.id) }}>{
+                                                    `${promoter.name}`
+                                                }</td>
+                                                <td className="trClickable" onClick={() => { history.push("/regulator/" + regulator.id) }}>{
+                                                    `${regulator.name} - ${regulator.type}`
+                                                }</td>
+                                                <td className="trClickable" onClick={() => { history.push("/tu/" + tu.id) }}>{
+                                                    tu.name
+                                                }</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
                     </div>
                 )
-            }catch(error){
+            } catch (error) {
                 return <> no data </>
             }
         }
     }
 }
- 
+
 export default TableOperon;
