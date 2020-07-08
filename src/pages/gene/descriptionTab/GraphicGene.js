fd4dea04ec524581a2f9596b7e5729375b6c5a8e
Gabriel CCG <galarcon@ccg.unam.mx>
2020-07-07 23:17:56 -0500

    add panzoom library

--- src/pages/gene/descriptionTab/GraphicGene.js
+++ src/pages/gene/descriptionTab/GraphicGene.js
@@ -2,26 +2,31 @@ import React, { useState } from 'react';
 import Image from '../../components/ui-components/infoDisplay/media/Image'
 import img from '../../../img/gene_context.png'
 import imgZ from '../../../img/gene_zoom.png'
+import { PanZoom } from 'react-easy-panzoom'
+
 const GraphicGene = ({
     idGene
 }) => {
     const [zoom, setZoom] = useState(false)
-
 
     return (
         <table>
             <tbody>
                 <tr>
                     <td>
-                    <div onClick={() => { setZoom(!zoom) }} style={{ cursor: "grab", width: "100%", textAlign: "center"  }}>
-                {
-                    zoom
-                        ? <Image id={idGene} urlImage={imgZ} imgStyle={{ maxWidth: "100%", height: "120px" }} />
-                        : <Image id={idGene} urlImage={img} imgStyle={{ maxWidth: "100%", height: "120px" }} />
-                }
-
-            </div>
+                        <div style={{ textAlign: "center" }}>
+                            <PanZoom>
+                                {
+                                    zoom
+                                        ? <Image id={idGene} urlImage={imgZ} imgStyle={{ maxWidth: "100%", height: "120px" }} />
+                                        : <Image id={idGene} urlImage={img} imgStyle={{ maxWidth: "100%", height: "120px" }} />
+                                }
+                            </PanZoom>
+                        </div>
                     </td>
+                </tr>
+                <tr>
+                    <td></td>
                 </tr>
             </tbody>
         </table>
