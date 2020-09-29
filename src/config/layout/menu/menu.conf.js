3a1052e446513cdaee3475dfd3e08d1c76b9b17a
Gabriel CCG <galarcon@ccg.unam.mx>
2020-09-29 13:59:37 -0500

    add download sets menu

--- src/config/layout/menu/menu.conf.js
+++ src/config/layout/menu/menu.conf.js
@@ -2,12 +2,14 @@
 import home from './home.conf.json'
 import tools from './tools.conf.json'
 import overviews from './overviews.conf.json'
+import downloadSets from './downloadSet.conf.json'
 
 const menuConf = {
     "menu": [
         home,
         tools,
-        overviews
+        overviews,
+        downloadSets
     ]
 }
 
