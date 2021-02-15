b5c011cb83bb55b4d528fdcc0c2239e706806b30
Gabriel CCG <galarcon@ccg.unam.mx>
2021-02-15 14:23:02 -0600

    refact search p1

---
+++ src/apps/search/search_result.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Cover } from "../../components/ui-components/ui_components";
+import SearchGenes from "./tools/search_genes";
+import conf from "./conf/search.conf.json"
+
+const SearchResults = ({ keyword }) => {
+  const [_genFound, set_geneFound] = useState();
+  const [_state, set_state] = useState("");
+  let state = _state;
+  let title = "Search Tool search result";
+  switch (_state) {
+    case "loading":
+      title = `Searching "${keyword}" Information`;
+      break;
+    case "done":
+      let totalResults = _genFound + 0;
+      title = `Search results for "${keyword}" (${_genFound})`;
+      if (totalResults === 0) {
+        title = "We did not find any results";
+        state = "error";
+      }
+      break;
+    case "error":
+      title = "Sorry we have technical difficulties, please try again later";
+      break;
+    default:
+      break;
+  }
+  return (
+    <>
+      <Cover state={state}>
+        <h1 assistentvalue={"Pagina de Resultado de Busqueda"}>{title}</h1>
+      </Cover>
+      <article>
+        <br />
+        {CollectionIndex(conf.collections, _genFound)}
+        <br />
+        <br />
+
+        <SearchGenes
+          geneStatus={(state) => {
+            set_state(state);
+          }}
+          keyword={keyword}
+          geneFounds={(nGene) => {
+            set_geneFound(nGene);
+          }}
+        />
+      </article>
+    </>
+  );
+};
+
+function CollectionIndex(collections, nGene) {
+  return (
+    <table style={{ width: "100%" }}>
+      <tbody>
+        <tr>
+          {collections.map((collection) => {
+            return (
+              <td key={`collections_${collection}`}>
+                <a href={`#table${collection}`}>{`${collection} (${nGene})`}</a>
+              </td>
+            );
+          })}
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default SearchResults;
