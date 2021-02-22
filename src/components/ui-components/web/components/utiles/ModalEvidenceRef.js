928753f510beaa49dc4dcc1b973edd2081954285
Gabriel CCG <galarcon@ccg.unam.mx>
2021-02-22 14:44:57 -0600

    fix undefined string on eveRef

--- src/components/ui-components/web/components/utiles/ModalEvidenceRef.js
+++ src/components/ui-components/web/components/utiles/ModalEvidenceRef.js
@@ -7,31 +7,44 @@ const ModalER = ({
     classNameModal,
     k,
 }) => {
+    let evidence = ""
+    let references = ""
     const evi = cit?.evidence
     const pub = cit?.publication
     let styleStrong = {}
     try {
-    if (evi?.type === "Strong") {
-        styleStrong = { fontWeight: "bold" }
-    }
-        const eviref = `
+        if (evi?.type === "Strong") {
+            styleStrong = { fontWeight: "bold" }
+        }
+        if (evi?.code) {
+            evidence = `
             <h2>Evidence</h1>
-            <h1>${evi?.code}: ${evi?.name}
-            <br>(${evi?.type})
+            <h1>${evi?.code}: ${validateStr(evi?.name)}
+            <br>(${validateStr(evi?.type)})
             </h1>
+            `
+        }
+        if(pub?.citation){
+            references = `
             <h2>Reference:</h2>
-            <h2><a href="${pub?.url}" target="_blank" rel="noopener noreferrer" >Go to Refence</a></h2>
-            <p>pmid:${pub?.pmid}</p>
+            <h2><a href="${validateStr(pub?.url)}" target="_blank" rel="noopener noreferrer" >Go to Refence</a></h2>
+            <p>pmid:${validateStr(pub?.pmid)}</p>
             <p class="citation">${pub?.citation}</p>
         `
-    return ( 
-        <Modal className={`aBase ${classNameModal}`} style={styleStrong} title={title} info={eviref} />
-     );
+        }
+        return (
+            <Modal className={`aBase ${classNameModal}`} style={styleStrong} title={title} info={`${evidence} ${references}`} />
+        );
     } catch (error) {
-        console.log("edivence error: ",error)
-        return ( <></> );
+        console.log("edivence error: ", error)
+        return (<></>);
     }
-    
+
 }
 
 export default ModalER;
+
+function validateStr(str = undefined) {
+    if (str) return str;
+    return ""
+}
