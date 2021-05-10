ec01c9243a13771edc887efce26e0330438fd042
Gabriel Alarcon Carranza <galarcon@ccg.unam.mx>
2021-05-10 14:29:48 -0500

    add table on operon home

--- src/apps/operon/operon_home.jsx
+++ src/apps/operon/operon_home.jsx
@@ -8,66 +8,53 @@ const Home = ({ conf }) => {
   const [_state, set_state] = useState();
   const [_limit, set_limit] = useState(5);
   const [_page, set_page] = useState(0);
-  console.log(_state)
-  switch (_state) {
-    case "loading":
+  if (_state === "done") {
+    if (_data != null) {
       return (
         <div>
           <Description
-            conf={conf} 
-            limit={_limit} 
-            setLimit={(limit)=>{set_limit(limit)}} 
+            conf={conf}
+            limit={_limit}
+            setLimit={(limit) => { set_limit(limit); set_state("loading")}}
           />
-            loading
+          <Table data={dataFormat(_data)} />
         </div>
       )
-    case "error":
+      //<Table data={dataFormat(_data)} />
+    } else {
       return (
         <div>
-          <Description
-            conf={conf} 
-            limit={_limit} 
-            setLimit={(limit)=>{set_limit(limit)}} 
-          />
-            error
+          error
         </div>
       )
-    case "done":
-      if (_data != null) {
-
-        return (
-          <div>
-            <Description
-              conf={conf} 
-              limit={_limit} 
-              setLimit={(limit)=>{set_limit(limit)}} 
-            />
-            
-          </div>
-        )
-        //<Table data={dataFormat(_data)} />
-      } else {
-        return (
-          <div>
-            <Description
-              conf={conf} 
-              limit={_limit} 
-              setLimit={(limit)=>{set_limit(limit)}} 
-            />
-            error
-          </div>
-        )
-      }
-    default:
-      break;
+    }
   }
   return (
-    <AllOperon
-      resoultsData={(data) => { set_data(data) }}
-      resoultsFound={(find) => { set_find(find) }}
-      status={(state)=>{set_state(state)}}
-      limit={_limit} page={_page}
-    />
+    <div>
+      <AllOperon
+        resoultsData={(data) => { set_data(data) }}
+        resoultsFound={(find) => { set_find(find) }}
+        status={(state) => { set_state(state) }}
+        limit={_limit} page={_page}
+      />
+      <Description
+        conf={conf}
+        limit={_limit}
+        setLimit={(limit) => { set_limit(limit) }}
+      />
+      {
+        _state==="loading"
+        ?"loading"
+        :""
+      }
+      {
+        _state==="error"
+        ?"error"
+        :""
+      }
+      
+    </div>
+
   );
 };
 
@@ -76,17 +63,24 @@ export default Home;
 class Description extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {value: this.props.limit}
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.props.setLimit(event.target.value);
+    let limit = event.target.value
+    console.log(limit)
+    if(limit<=0){
+      this.setState({value:0})
+    }else{
+      this.props.setLimit(limit);
+    }
+    
   }
 
   render() {
     const {
       conf,
-      limit,
     } = this.props
     return (
       <>
@@ -96,8 +90,8 @@ class Description extends React.Component {
         Displayed Operons:
         <br />
         <input type="number"
-          value={limit}
-          onChange={this.handleChange} 
+          value={this.state.value}
+          onChange={this.handleChange}
         />
         <br />
       </>
@@ -110,11 +104,12 @@ function dataFormat(data) {
   let rows = [];
   if (data) {
     data.map((doc) => {
+      console.log(doc)
       const id = doc?._id;
       const tus = doc?.transcriptionUnits;
       const d = {
         name: `${doc?.operon?.name} operon`,
-        tus: `contains ## genes organized in ${tus.length} TUs`,
+        tus: `contains ${doc?.operon?.statistics?.genes} genes organized in ${tus.length} TUs`,
         id: id
       };
       rows.push(d);
