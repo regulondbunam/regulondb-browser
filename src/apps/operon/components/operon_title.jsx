0d04c3bef34a7b466eb3ad6f9621e9eeb9a8dcb9
Gabriel Alarcon Carranza <galarcon@ccg.unam.mx>
2021-06-11 14:58:22 -0500

    add loading timer operon info

--- src/apps/operon/components/operon_title.jsx
+++ src/apps/operon/components/operon_title.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import { Cover} from "../../../components/ui-components/ui_components";
+import React, { useState, useEffect } from 'react'
+import { Cover } from "../../../components/ui-components/ui_components";
 
 
 export const Title = () => {
@@ -8,22 +8,26 @@ export const Title = () => {
     const [_title, set_title] = useState("");
     useEffect(() => {
         const cover = document.getElementById("div_cover_operon_01")
-        if(cover){
-            cover.addEventListener('coverR', function (e) { 
+        if (cover) {
+            cover.addEventListener('coverR', function (e) {
                 console.log(`state`, e.detail)
-                set_state(e.detail.state)
-                set_title(e.detail.title)
-                if(e.detail.isInfo){
+                if (e.detail.state) {
+                    set_state(e.detail.state)
+                }
+                if (e.detail.title) {
+                    set_title(e.detail.title)
+                }
+                if (e.detail.isInfo) {
                     set_isInfo(true)
                 }
-             }, false);
+            }, false);
         }
     }, [])
     return (
         <div id={"div_cover_operon_01"}>
             <Cover id="component_cover_operon_01" state={_state}>
-                {_isInfo?"operon":""}
-                    <h1 id="cover_operon_01" >{_title}</h1>
+                {_isInfo ? "operon" : ""}
+                <h1 id="cover_operon_01" >{_title}</h1>
             </Cover>
         </div>
     )
--- src/apps/operon/operon_info.jsx
+++ src/apps/operon/operon_info.jsx
@@ -3,12 +3,28 @@ import React, { useState, useEffect } from 'react'
 
 import Tabs from "./components/operon_tabs";
 import Conf from './conf/confRaw.conf'
-import Title from './components/operon_title'
-//import conf from './conf/operon.conf.json'
-//const info = conf.pages.operon_info
 
 export const Info = ({ id, tuId, nTUs}) => {
+    const [_state, set_state] = useState("loading");
     const [_conf, set_conf] = useState()
+
+    useEffect(() => {
+        const covera = document.getElementById("div_cover_operon_01")
+        if(covera){
+            const coverR = new CustomEvent('coverR',{
+                bubbles: true,
+                detail: { 
+                    state: _state,
+                }
+            });
+            covera.dispatchEvent(coverR);
+        }
+        if(_state !== "done"){
+            setTimeout(function(){
+                set_state("done")
+            },5000);
+        }
+    })
 
     if (id && _conf) {
         return (
