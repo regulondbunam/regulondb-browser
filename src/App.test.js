e8aa8d6a0a41bdda494e7499869eb3f79f86f6dd
Gabriel CCG <galarcon@ccg.unam.mx>
2021-07-19 09:05:37 -0500

    delete old files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Title RegulonDB Browser', () => {
-  render(<App />);
-  const linkElement = screen.getByText('The RegulonDB Browser');
-  expect(linkElement).toBeInTheDocument();
-});
--- src/apps/operon/webServices/operon_ws.js
+++
@@ -1,8 +0,0 @@
-import GetAll from './operon/operon_all'
-import GetArrangement from './operon/operon_arrangement'
-import GetCitations from "./operon/operon_citations";
-import GetInfo from './operon/operon_info'
-import GetStatistics from './operon/operon_statisitics'
-import ValidateID from './operon/operon_validate'
-
-export {GetAll, GetArrangement, GetCitations, GetInfo, GetStatistics, ValidateID}
