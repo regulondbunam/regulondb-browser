3baba6ef1e8049597e5312b61b4969aa00cc2c05
Gabriel CCG <galarcon@ccg.unam.mx>
2021-07-30 23:24:46 -0500

    add regulon config file

---
+++ src/apps/regulon/components/regulon_tabs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Tabs } from "../../../components/ui-components/ui_components";
+
+export default function RegulonTabs() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
