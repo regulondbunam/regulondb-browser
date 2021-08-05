dc0a041d4f954bac40ac4a92125f99ce8f7293e7
Gabriel CCG <galarcon@ccg.unam.mx>
2021-08-05 10:24:39 -0500

    update gene home page

---
+++ src/apps/gene/components/gene_table.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+const styleTh = {
+    fontWeight: "bold",
+    borderBottom: "1px solid rgb(114, 167, 199)",
+    textAlign: "inherit",
+}
+
+export default function Table(genes = []) {
+
+    const history = useHistory()
+
+    if (genes.length === 0 || !genes) {
+        return null
+    }
+
+    return (
+        <table>
+            <thead>
+                <tr style={styleTh} >  
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Synonyms
+                    </th>
+                    <th>
+                        Products
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    genes.map((gene)=>{
+                        let products = ""
+                        try {
+                            products = gene?.products.map(pro=>{
+                                return pro?.name
+                            }).join(", ")
+                        } catch (error) {
+                            
+                        }
+                        return(
+                            <tr className="trClickable" key={`table_gene_${gene?.id}`} onClick={()=>{history.push(`/gene/${gene.id}`)}}  >
+                                <td dangerouslySetInnerHTML={{__html: gene?.gene?.name}} />
+                                <td dangerouslySetInnerHTML={{__html: gene?.gene?.synonyms}}/>
+                                <td dangerouslySetInnerHTML={{__html: products}}/>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    )
+}
