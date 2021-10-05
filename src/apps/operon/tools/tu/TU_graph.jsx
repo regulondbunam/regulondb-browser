d7c7a58c29c054a1a13c609646ccb5a82dde16a3
galarcon-ccg <galarcon@ccg.unam.mx>
2021-10-04 20:52:06 -0500

    add terminator in tu graph

--- src/apps/operon/tools/tu/TU_graph.jsx
+++ src/apps/operon/tools/tu/TU_graph.jsx
@@ -4,6 +4,7 @@ import DttTool from '../../../../components/dtt_tool/dttTool'
 
 export default function TUgraph({ data }) {
     const { operonContextElements } = useContext(OperonCONTEXT)
+    //console.log(data)
     //console.log(operonContextElements)
     let custom_dnaFeatures = useMemo(() => {
         try {
@@ -29,19 +30,34 @@ export default function TUgraph({ data }) {
                     case "tf_binding_site":
                         dnaFeatures.push(element)
                         break;
+                    case "terminator":
+                        if(!data?.terminators || data?.terminators.length === 0){
+                            return null
+                        }
+                        try {
+                            data?.terminators.map((term)=>{
+                                if (term._id === element._id) {
+                                    dnaFeatures.push(element)
+                                }
+                                return null
+                            })
+                        } catch (error) {
+                            return null
+                        }
+                        break;
                     default:
                         break;
                 }
                 return null
             })
             let tu_data = []
-            dnaFeatures.map(feature=>{
-                if(feature?.objectType === "tf_binding_site"){
+            dnaFeatures.map(feature => {
+                if (feature?.objectType === "tf_binding_site") {
                     let genes = feature?.relatedGenes
-                    if(genes){
+                    if (genes) {
                         let flag = false
-                        genes.map(gene=>{
-                            if(dnaFeatures.find(f=>f?._id===gene.gene_id)){
+                        genes.map(gene => {
+                            if (dnaFeatures.find(f => f?._id === gene.gene_id)) {
                                 flag = true
                             }
                             return null
@@ -55,16 +71,16 @@ export default function TUgraph({ data }) {
                 tu_data.push(feature)
                 return null
             })
-            return  tu_data
+            return tu_data
         } catch (error) {
             console.error("TU_graph: ", error)
         }
-    },[data,operonContextElements])
-    
+    }, [data, operonContextElements])
+
     return (
-            <DttTool id={data.id} context="tu"
-                custom_data_dtt={custom_dnaFeatures}
-            />
+        <DttTool id={data.id} context="tu"
+            custom_data_dtt={custom_dnaFeatures}
+        />
     )
 }
 
