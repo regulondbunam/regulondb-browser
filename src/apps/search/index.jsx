c3c048f675683c6548b64b1c6fec832277e0f03f
galarcon-ccg <galarcon@ccg.unam.mx>
2022-08-25 10:54:53 -0500

    fix search tool bug

--- src/apps/search/index.jsx
+++ src/apps/search/index.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Main from './main/Main';
 import Results from './results/Results';
 
 function Search() {
-    let { keyword } = useParams() 
-    if (keyword) {
-        return ( 
-            <Results keyword={keyword} />
-         );
-    }
-    return <Main />;
+    const [_keyword, set_keyword] = useState();
+    let { keyword } = useParams()
+    useEffect(() => {
+        if (keyword !== _keyword) {
+          if (!_keyword) {
+            set_keyword(keyword);
+          } else {
+            set_keyword(undefined);
+          }
+        }
+      }, [_keyword, keyword]);
+    return(
+        <div>
+            {
+                !_keyword && (<Main />)
+            }
+            {
+                _keyword && (<Results keyword={keyword} />)
+            }
+        </div>
+    )
 }
 
 export default Search;
