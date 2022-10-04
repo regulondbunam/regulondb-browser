74d5f93507f58c8a1bcd701c1ceedce4359d8210
galarcon-ccg <galarcon@ccg.unam.mx>
2022-10-04 16:21:19 -0500

    add scroll focus dtt

--- src/apps/dtt/rdb_data/index.jsx
+++ src/apps/dtt/rdb_data/index.jsx
@@ -6,34 +6,40 @@ import DrawingTracesTool from "../../../components/DrawingTracesTool";
 function RDBdata() {
   const [_formData, set_formData] = useState();
   const [_geneticElements, set_geneticElements] = useState();
+  const _height = window.innerHeight / 2;
+
   return (
     <div>
-      <div style={{marginLeft: "10%",marginRight: "10%"}}>
+      <div style={{ marginLeft: "10%", marginRight: "10%" }}>
         <h2>Drawing Traces from RegulonDB</h2>
         <Form
           onDraw={(formData) => {
             set_formData(formData);
           }}
-          onReset={()=>{set_formData(undefined);set_geneticElements(undefined)}}
+          onReset={() => {
+            set_formData(undefined);
+            set_geneticElements(undefined);
+          }}
         />
         <br />
-        {_geneticElements && <Table geneticElements={_geneticElements} />}
       </div>
-      <div>
-
-      </div>
+      {_geneticElements && (
+        <Table height={_height} geneticElements={_geneticElements} />
+      )}
       {_formData && (
         <div>
           <DrawingTracesTool
             id={"rdb_dti_001"}
             context={"dti"}
-            height={300}
+            height={_height}
             leftEndPosition={_formData.leftEndPosition}
             rightEndPosition={_formData.rightEndPosition}
             strand={_formData.strand}
             covered={_formData.covered}
             objectType={_formData.objectType}
-            getGeneticElements={(ge)=>{set_geneticElements(ge)}}
+            getGeneticElements={(ge) => {
+              set_geneticElements(ge);
+            }}
           />
         </div>
       )}
--- src/apps/dtt/rdb_data/table/index.jsx
+++ src/apps/dtt/rdb_data/table/index.jsx
@@ -1,19 +1,27 @@
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function Table({geneticElements}) {
+function Table({geneticElements,height = "100px"}) {
     const [_show, set_show] = useState(true);
     console.log(geneticElements)
+
+    useEffect(() => {
+      let section = document.getElementById("rdb_table_GE");
+      if (section) {
+          console.log("hola");
+        let rect = section.getBoundingClientRect();
+        window.scroll({
+          top: rect.y + window.pageYOffset,
+          behavior: "smooth",
+        });
+      }
+    }, []);
+
     return ( 
-        <Box
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-      >
+        <div id="rdb_table_GE" >
         <div className="rdb_form_title">
           <IconButton
             sx={{ width: "10px", height: "10px" }}
@@ -27,10 +35,10 @@ function Table({geneticElements}) {
           <h3>Genetic elements data table</h3>
         </div>
         {_show && (
-          <Paper elevation={3} sx={{ padding: "5px" }}>
+          <Paper elevation={3} sx={{ padding: "5px", height: height }}>
             </Paper>
         )}
-        </Box>
+        </div>
      );
 }
 
