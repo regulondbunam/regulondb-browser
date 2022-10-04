772d02815d1ec745bb387fade9a35275c13ed4fd
galarcon-ccg <galarcon@ccg.unam.mx>
2022-10-04 15:20:59 -0500

    update dtt

--- src/apps/dtt/rdb_data/index.jsx
+++ src/apps/dtt/rdb_data/index.jsx
@@ -1,14 +1,43 @@
+import React, { useState } from "react";
 import Form from "./form";
+import Table from "./table";
+import DrawingTracesTool from "../../../components/DrawingTracesTool";
 
 function RDBdata() {
-    return ( 
+  const [_formData, set_formData] = useState();
+  const [_geneticElements, set_geneticElements] = useState();
+
+  return (
+    <div>
+      <article>
+        <h2>Drawing Traces from RegulonDB</h2>
+        <Form
+          onDraw={(formData) => {
+            set_formData(formData);
+          }}
+          onReset={()=>{set_formData(undefined);set_geneticElements(undefined)}}
+        />
+        <br />
+        {_geneticElements && <Table />}
+        <br />
+      </article>
+
+      {_formData && (
         <div>
-            <h2>
-                Drawing Traces from RegulonDB
-            </h2>
-            <Form />
+          <DrawingTracesTool
+            id={"rdb_dti_001"}
+            context={"dti"}
+            height={300}
+            leftEndPosition={_formData.leftEndPosition}
+            rightEndPosition={_formData.rightEndPosition}
+            strand={_formData.strand}
+            covered={_formData.covered}
+            objectType={_formData.objectType}
+          />
         </div>
-     );
+      )}
+    </div>
+  );
 }
 
 export default RDBdata;
