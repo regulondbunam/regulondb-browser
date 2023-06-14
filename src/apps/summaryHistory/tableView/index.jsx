9de1fbeb9318e055967f8ab2f7112108c86abdf0
Elizabeth Ochoa <elizabethochoap23@gmail.com>
2023-06-14 10:51:54 -0600

    Funcion para iterar genomicObjects

--- src/apps/summaryHistory/tableView/index.jsx
+++ src/apps/summaryHistory/tableView/index.jsx
@@ -43,7 +43,7 @@ export default function TableView({ arraySummary = [] }) {
 function formatTable(data = []) {
 
     let tableData = { rows: [], colums: [] }
-    //logica
+
     let colums = data.map((row) => {
         return `${row.regulonDBVersion} ${row.releaseDate}`
     })
@@ -51,6 +51,7 @@ function formatTable(data = []) {
 
     let rows = []
     let genomicObjects = {}
+    let products = {}
     data.forEach((summary) => {
 
         //Creacion Filas
@@ -58,23 +59,47 @@ function formatTable(data = []) {
             const statistics = summary.statistics
             Object.keys(statistics).map((key) => {
                 const statistic = statistics[key]
-                if(!genomicObjects.hasOwnProperty(key)){
+                if (!genomicObjects.hasOwnProperty(key)) {
                     genomicObjects[key] = []
                 }
                 if (validObject(statistic)) {
                     if (statistic.__typename === "detailedStatistics") {
                         genomicObjects[key].push(statistic.total)
                     }
-                    //condicion si typename es dbInfoExternalReferencesType
-                    //condicion si typename es  productsDBInfoType
-                    //condicion si typename es dbInfoRegulons 
+                    if (statistic.__typename === "dbInfoRegulons") {
+                        genomicObjects[key].push(statistic.total)
+                    }
+                    if (statistic.__typename === "productsDBInfoType") {
+                        Object.keys(statistics.product).map((key) => {
+                            //validar si el key de product esta dentro de genomicObjects
+                            if (!genomicObjects.hasOwnProperty(key)) {
+                                genomicObjects[key] = []
+                            }
+                            const product = statistics.product[key]
+                            if (validObject(product)) {
+                                genomicObjects[key].push(product.total)
+                                //console.log(products)
+                            }
+                        })
+                        genomicObjects[key].push(null)
+                    }
+                    if (statistic.__typename === "dbInfoExternalReferencesType") {
+                        genomicObjects[key].push(statistic.total)
+                    }
                 }
             })
-
-
         }
-        //row = {attenuators: [128,3875,192],effectors: [12,123,434,65]}
     })
+    /**
+     *row = [
+        ["attenuators",2,12,4,45,64,3,23,4235,234,243123,14,1243,354 ...],
+        ["effectors",2,12,4,45,64,3,23,4235,234,243123,14,1243,354 ...],
+     ]
+     1 -> Iterar Objeto (Object.keys(genomicObjects))
+        1.2-> dentro de iteracion crear el arreglo [key, ...arrglo] utilizando destructuracion https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+        1.3 -> condicion si key es product crear el formato para  polipeptides, srna, rnas
+        1.3 -> aniadir al arreglo row con un push()
+     */
     console.log(genomicObjects)
 
     tableData.rows.push(rows)
