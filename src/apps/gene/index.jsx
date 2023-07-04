1e7339f5b06e18068cb6db176b24df4d81d3aba6
galarcon-ccg <galarcon@ccg.unam.mx>
2023-07-04 08:34:08 -0600

    fix error in gene home

--- src/apps/gene/index.jsx
+++ src/apps/gene/index.jsx
@@ -8,34 +8,35 @@ import Home from "./Home";
 
 export default function Gene() {
   let { geneId } = useParams();
-  if(geneId.split("_").length>1){
-    geneId = geneId.split("_")[0]
-  }
-  if(geneId){
+
+  if (geneId) {
+    if (geneId.split("_").length > 1) {
+      geneId = geneId.split("_")[0]
+    }
     return <GeneDescription geneId={geneId} />
   }
   return <Home />
 }
 
-function GeneDescription({geneId}){
+function GeneDescription({ geneId }) {
 
   const [title, setTitle] = React.useState("gene");
-  const {geneData, loading, error} = useGetGenesBy({_id: geneId})
+  const { geneData, loading, error } = useGetGenesBy({ _id: geneId })
   console.log(geneData);
   useEffect(() => {
     if (loading) {
-      UpdateTitle({state: "loading"})
+      UpdateTitle({ state: "loading" })
       setTitle("Loading gene data...")
     }
     if (geneData && !error) {
-      UpdateTitle({state: "done"})     
+      UpdateTitle({ state: "done" })
     }
     if (geneData === null) {
-      UpdateTitle({state:"error"})
+      UpdateTitle({ state: "error" })
       setTitle(`Sorry, Gene:${geneId} don't found`)
     }
-    if(error){
-      UpdateTitle({state:"error"})
+    if (error) {
+      UpdateTitle({ state: "error" })
       setTitle(`Error to query ${geneId} information`)
     }
   }, [geneData, loading, error, geneId]);
@@ -44,7 +45,7 @@ function GeneDescription({geneId}){
     <div>
       <Title title={title} geneData={geneData} />
       {geneData && (
-         <Information geneData={geneData}  />
+        <Information geneData={geneData} />
       )}
     </div>
   )
