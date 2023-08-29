17c3e576098dde61bff5d1605eaac7bb61ee09c3
Galarcon128 <alarcongabriel128@gmail.com>
2023-08-28 22:55:28 -0600

    update RI table in regulon

--- src/apps/regulon/_oldDetails/regulatoryInteractions/index.jsx
+++ src/apps/regulon/_oldDetails/regulatoryInteractions/index.jsx
@@ -1,55 +1,209 @@
-import React from 'react';
-import Paper from "@mui/material/Paper";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import IconButton from "@mui/material/IconButton";
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import NativeSelect from '@mui/material/NativeSelect';
-import { Extended } from './extended';
-import { Table } from './ViewTable';
+import React, { useMemo } from "react";
+import {
+  FilterTable,
+  DataVerifier,
+} from "../../../../components/ui-components";
+import { Link } from "react-router-dom";
+import { ParagraphCitations } from "../../../../components/datamartSchema/citations/paragraph";
+
+const COLUMNS = [
+  {
+    id: "regulatedEntity",
+    header: "Regulated Entity",
+    columns: [
+      {
+        id: "regulatedEntity_name",
+        header: "name",
+        accessorKey: "_regulatedEntity_name",
+      },
+      {
+        id: "regulatedEntity_type",
+        header: "type",
+        accessorKey: "_regulatedEntity_type",
+      },
+    ],
+  },
+  {
+    id: "activeConformation",
+    header: "Active Conformation",
+    columns: [
+      {
+        id: "activeConformation_name",
+        header: "name",
+        accessorKey: "_activeConformation_name",
+      },
+      {
+        id: "activeConformation_type",
+        header: "type",
+        accessorKey: "_activeConformation_type",
+      },
+    ],
+  },
+  {
+    id: "regulatedGenes",
+    header: "Regulated Gene",
+    columns: [
+      {
+        id: "regulatedGenes_name",
+        header: "name",
+        accessorKey: "_regulatedGenes_name",
+        cell: (info) => {
+          const genes = info.row.original.regulatedGenes;
+          return (
+            <>
+              {genes.map((gene) => (
+                <Link
+                  key={
+                    "ri_" + info.row.original.id + "_regulatedGene_" + gene._id
+                  }
+                >
+                  <p dangerouslySetInnerHTML={{ __html: gene.name }} />
+                </Link>
+              ))}
+            </>
+          );
+        },
+      },
+    ],
+  },
+  {
+    id: "regulatoryInteraction",
+    header: "Regulatory Interaction",
+    columns: [
+      {
+        id: "regulatoryInteraction_function",
+        header: "Function",
+        accessorKey: "_regulatoryInteraction_function",
+      },
+    ],
+  },
+  {
+    id: "regulatoryInteraction_distanceTo",
+    header: "Distance to",
+    columns: [
+      {
+        id: "regulatoryInteraction_distanceTo_gene",
+        header: "First Gene",
+        accessorKey: "_distanceGene",
+      },
+      {
+        id: "regulatoryInteraction_distanceTo_promoter",
+        header: "Promoter",
+        accessorKey: "_distancePromoter",
+      },
+    ],
+  },
+  {
+    id: "regulatoryBindingSite",
+    header: "Regulatory BindingSite",
+    columns: [
+      {
+        id: "regulatoryBindingSite_position",
+        header: "Position",
+        accessorKey: "_regulatoryBindingSite_position",
+      },
+      {
+        id: "regulatoryBindingSite_strand",
+        header: "Strand",
+        accessorKey: "_regulatoryBindingSite_strand",
+      },
+      {
+        id: "regulatoryBindingSite_sequence",
+        header: "Sequence",
+        accessorKey: "_regulatoryBindingSite_sequence",
+      },
+    ],
+  },
+  {
+    id: "citations",
+    header: "Citations",
+    accessorKey: "_citations",
+    cell: (info) => {
+      const citations = info.row.original.citations;
+      const allCitations = info.row.original.allCitations;
+      return (
+        <ParagraphCitations allCitations={allCitations} citations={citations} />
+      );
+    },
+  },
+];
+
+function formatData(regulatoryInteractions = [], allCitations) {
+  let data = [];
+  if (DataVerifier.isValidArray(regulatoryInteractions)) {
+    regulatoryInteractions.forEach((ri, index) => {
+      let _regulatedEntity_name = "";
+      let _regulatedEntity_type = "";
+      if (DataVerifier.isValidObject(ri.regulatedEntity)) {
+        _regulatedEntity_name = ri.regulatedEntity.name;
+        _regulatedEntity_type = ri.regulatedEntity.type;
+      }
+      let _activeConformation_name = "";
+      let _activeConformation_type = "";
+      if (DataVerifier.isValidObject(ri.activeConformation)) {
+        _activeConformation_name = ri.activeConformation.name;
+        _activeConformation_type = ri.activeConformation.type;
+      }
+      let _regulatedGenes_name = "";
+      let regulatedGenes = [];
+      if (DataVerifier.isValidArray(ri.regulatedGenes)) {
+        _regulatedGenes_name = ri.regulatedGenes
+          .map((gene) => gene.name)
+          .join("; ");
+        regulatedGenes = ri.regulatedGenes;
+      }
+      let _regulatoryInteraction_function = ri.function;
+      let _distanceGene = ri.distanceToFirstGene;
+      let _distancePromoter = ri.distanceToPromoter;
+      let citations = ri.citations;
+      let _regulatoryBindingSite_position = "";
+      let _regulatoryBindingSite_strand = "";
+      let _regulatoryBindingSite_sequence = "";
+      if (DataVerifier.isValidObject(ri.regulatoryBindingSites)) {
+        if (
+          DataVerifier.isValidNumber(ri.regulatoryBindingSites.leftEndPosition)
+        ) {
+          _regulatoryBindingSite_position =
+            ri.regulatoryBindingSites.leftEndPosition +
+            " - " +
+            ri.regulatoryBindingSites.rightEndPosition;
+        }
+        _regulatoryBindingSite_strand = ri.regulatoryBindingSites.strand;
+        _regulatoryBindingSite_sequence = ri.regulatoryBindingSites.sequence;
+        if (DataVerifier.isValidArray(ri.regulatoryBindingSites.citations)) {
+          if (ri.regulatoryBindingSites.citations.length > citations.length)
+            citations = ri.regulatoryBindingSites.citations;
+        }
+      }
+      data.push({
+        id: "ri_" + index + "_" + ri._id,
+        _regulatedEntity_name: _regulatedEntity_name,
+        _regulatedEntity_type: _regulatedEntity_type,
+        _activeConformation_name: _activeConformation_name,
+        _activeConformation_type: _activeConformation_type,
+        _regulatedGenes_name: _regulatedGenes_name,
+        regulatedGenes: regulatedGenes,
+        _regulatoryInteraction_function: _regulatoryInteraction_function,
+        _distanceGene: _distanceGene,
+        _distancePromoter: _distancePromoter,
+        _regulatoryBindingSite_position: _regulatoryBindingSite_position,
+        _regulatoryBindingSite_strand: _regulatoryBindingSite_strand,
+        _regulatoryBindingSite_sequence: _regulatoryBindingSite_sequence,
+        citations: citations,
+        allCitations: allCitations,
+        _citations: "",
+      });
+    });
+  }
+  return data;
+}
 
 function RegulatoryInteractions({ regulatoryInteractions, allCitations }) {
-
-    const VIEW_OPTIONS = ["Table", "Extended"]
-    const views = {
-        "Extended": <Extended regulatoryInteractions={regulatoryInteractions} allCitations={allCitations} />,
-        "Table": <Table regulatoryInteractions={regulatoryInteractions} allCitations={allCitations} />
-    }
-    const [_show, set_show] = React.useState(true);
-    const [view, setView] = React.useState(VIEW_OPTIONS[0]);
-
-    //console.log(regulatoryInteractions);
-
-    return (
-        <div style={{ margin: "0 5% 0 5%", padding: "0 0 20px 0" }} >
-            <div>
-                <FormControl >
-                    <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                        View Option
-                    </InputLabel>
-                    <NativeSelect
-                        defaultValue={view}
-                        inputProps={{
-                            name: 'view option',
-                            id: 'ri-viw-option',
-                        }}
-                        onChange={(event) => {
-                            setView(event.target.value)
-                        }}
-                    >
-                        {
-                            VIEW_OPTIONS.map((value, index) => {
-                                return <option key={"ri-viewOption-" + index + "-" + value} value={value}>{value}</option>
-                            })
-                        }
-                    </NativeSelect>
-                </FormControl>
-                {views[view]}
-            </div>
-        </div>
-
-    );
+  console.log(regulatoryInteractions);
+  const data = useMemo(() => {
+    return formatData(regulatoryInteractions, allCitations);
+  }, [regulatoryInteractions, allCitations]);
+  return <FilterTable data={data} columns={COLUMNS} />;
 }
 
 export default RegulatoryInteractions;
--- src/components/ui-components/utils/index.js
+++ src/components/ui-components/utils/index.js
@@ -1,70 +1,74 @@
-
-
 class DataVerifier {
-
-    static isValidObject(obj = {}){
-        if (!obj) {
-            return false
-        }
-        if (obj === null) {
-            return false
-        }
-        if (Object.keys(obj).length === 0) {
-            return false
-        }
-        return true
+  static isValidObject(obj = {}) {
+    if (!obj) {
+      return false;
     }
-
-    static isValidArray(array) {
-        if (!array) {
-            return false
-        }
-        if (array === null) {
-            return false
-        }
-        if (!Array.isArray(array)) {
-            return false
-        }
-        if (array.length <= 0) {
-            return false
-        }
-        return true
+    if (obj === null) {
+      return false;
     }
-
-    static isValidNumber(num){
-        if (!num) {
-            return false
-        }
-        if (num === null) {
-            return false
-        }
-        if (isNaN(num)) {
-            return false
-        }
-        if (Number.isInteger) {
-            if (!Number.isSafeInteger) {
-                console.error("no safe Integer", num);
-                return false
-            }
-        }
-        return true
+    if (Object.keys(obj).length === 0) {
+      return false;
     }
+    return true;
+  }
 
-    static isValidString(str) {
-        if (!str) {
-            return false
-        }
-        if (str === null) {
-            return false
-        }
-        if (str === "null") {
-            return false
-        }
-        if (str === "") {
-            return false
-        }
-        return true
+  static isValidArray(array) {
+    if (!array) {
+      return false;
     }
+    if (array === null) {
+      return false;
+    }
+    if (!Array.isArray(array)) {
+      return false;
+    }
+    if (array.length <= 0) {
+      return false;
+    }
+    return true;
+  }
+
+  static isValidNumber(num) {
+    if (!num) {
+      return false;
+    }
+    if (num === null) {
+      return false;
+    }
+    if (isNaN(num)) {
+      return false;
+    }
+    if (Number.isInteger) {
+      if (!Number.isSafeInteger) {
+        console.error("no safe Integer", num);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  static toValidString(str) {
+    if(this.isValidString(str+"")){
+        return str+""
+    }
+    return ""
+  }
+
+  static isValidString(str) {
+    if (!str) {
+      return false;
+    }
+    if (str === null) {
+      return false;
+    }
+    if (str === "null") {
+      return false;
+    }
+    if (str === "") {
+      return false;
+    }
+    return true;
+  }
 }
 
-export default DataVerifier
+export default DataVerifier;
