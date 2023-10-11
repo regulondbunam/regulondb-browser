c439354218316e6b4f27e2e3400a61d79bf0128c
Galarcon128 <alarcongabriel128@gmail.com>
2023-10-11 00:09:52 -0600

    add regulon lin on gene regulator

--- src/components/datamartSchema/gene/products/index.jsx
+++ src/components/datamartSchema/gene/products/index.jsx
@@ -83,12 +83,12 @@ Motif: highlight or display information about specific patterns within a given s
 **/
 
 import ViewSequence from "./viewSequence";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { DataVerifier, Accordion } from "../../../ui-components";
 import { ParagraphCitations, NoteCitations } from "../../citations";
-import GeneOntologyTerms from "./geneOntologyTerms"
+import GeneOntologyTerms from "./geneOntologyTerms";
 import Motif from "./motif";
-
+import { Link } from "react-router-dom";
 
 /**
  * Description placeholder
@@ -96,24 +96,23 @@ import Motif from "./motif";
  * @type {{ _id: any; anticodon: any; cellularLocations: any; citations: any; externalCrossReferences: any; geneOntologyTerms: any; isRegulator: any; isoelectricPoint: any; molecularWeight: any; motifs: any; ... 5 more ...; type: any; }}
  */
 const PROP_TYPES = {
-    _id: PropTypes.string.isRequired,
-    anticodon: PropTypes.string,
-    cellularLocations: PropTypes.arrayOf(PropTypes.string),
-    citations: PropTypes.array,
-    externalCrossReferences: PropTypes.array,
-    geneOntologyTerms: PropTypes.object,
-    isRegulator: PropTypes.bool,
-    isoelectricPoint: PropTypes.number,
-    molecularWeight: PropTypes.string,
-    motifs: PropTypes.array,
-    name: PropTypes.string,
-    note: PropTypes.string,
-    regulonId: PropTypes.string,
-    sequence: PropTypes.string,
-    synonyms: PropTypes.arrayOf(PropTypes.string),
-    type: PropTypes.string,
+  _id: PropTypes.string.isRequired,
+  anticodon: PropTypes.string,
+  cellularLocations: PropTypes.arrayOf(PropTypes.string),
+  citations: PropTypes.array,
+  externalCrossReferences: PropTypes.array,
+  geneOntologyTerms: PropTypes.object,
+  isRegulator: PropTypes.bool,
+  isoelectricPoint: PropTypes.number,
+  molecularWeight: PropTypes.string,
+  motifs: PropTypes.array,
+  name: PropTypes.string,
+  note: PropTypes.string,
+  regulonId: PropTypes.string,
+  sequence: PropTypes.string,
+  synonyms: PropTypes.arrayOf(PropTypes.string),
+  type: PropTypes.string,
 };
-
 
 /**
  * Description placeholder
@@ -141,107 +140,119 @@ const PROP_TYPES = {
  * @returns {React.JSX}
  */
 export function Product({
-    _id,
-    anticodon,
-    cellularLocations,
-    citations,
-    externalCrossReferences,
-    geneOntologyTerms,
-    isRegulator,
-    isoelectricPoint,
-    molecularWeight,
-    motifs,
-    name,
-    note,
-    regulonId,
-    sequence,
-    synonyms,
-    type,
-    allCitations
+  _id,
+  anticodon,
+  cellularLocations,
+  citations,
+  externalCrossReferences,
+  geneOntologyTerms,
+  isRegulator,
+  isoelectricPoint,
+  molecularWeight,
+  motifs,
+  name,
+  note,
+  regulonId,
+  sequence,
+  synonyms,
+  type,
+  allCitations,
 }) {
-    //console.log(DataVerifier.isValidArray(motifs));
-    return (
-        <div>
-            <p style={{ fontSize: "18px" }} ><b dangerouslySetInnerHTML={{__html: name}}/></p>
-            <table className="table_auto table_content">
-                <tbody>
-                    {DataVerifier.isValidArray(synonyms) && (
-                        <tr>
-                            <td style={{ fontWeight: "bold" }}>Synonyms: </td>
-                            <td><p dangerouslySetInnerHTML={{__html: synonyms.join(", ")}} /></td>
-                        </tr>
-                    )}
-                    {DataVerifier.isValidString(sequence) && (
-                        <tr>
-                            <td style={{ fontWeight: "bold" }}>Sequence:</td>
-                            <td>
-                                <ViewSequence
-                                    sequence={sequence}
-                                    title={`product: ${name}`}
-                                />
-                            </td>
-                        </tr>
-                    )}
-                    {DataVerifier.isValidString(molecularWeight) && (
-                        <tr>
-                            <td style={{ fontWeight: "bold" }}>Molecular Weight: </td>
-                            <td>{molecularWeight}</td>
-                        </tr>
-                    )}
-                    {DataVerifier.isValidString(anticodon) && (
-                        <tr>
-                            <td style={{ fontWeight: "bold" }}>Anticodon: </td>
-                            <td>{anticodon}</td>
-                        </tr>
-                    )}
-                    {isRegulator && (
-                        <tr>
-                            <td style={{ fontWeight: "bold" }}>Is regulator?:</td>
-                            <td>{isRegulator}</td>
-                        </tr>
-                    )}
-                    {DataVerifier.isValidNumber(isoelectricPoint) && (
-                        <tr>
-                            <td style={{ fontWeight: "bold" }}>Isoelectric Point: </td>
-                            <td>{isoelectricPoint}</td>
-                        </tr>
-                    )}
-                    {DataVerifier.isValidArray(cellularLocations) && (
-                        <tr>
-                            <td style={{ fontWeight: "bold" }}>Cellular Locations:</td>
-                            <td>{cellularLocations.join(", ")}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-            {DataVerifier.isValidString(note) && (
-                <Accordion title={<p style={{ fontWeight: "bold" }}>Note</p>} >
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: NoteCitations(allCitations, note),
-                        }}
-                    />
-                </Accordion>
-            )}
-            {DataVerifier.isValidObject(geneOntologyTerms) && (
-                <GOT geneOntologyTerms={geneOntologyTerms} allCitations={allCitations} />
-            )}
-            {DataVerifier.isValidArray(motifs) && (
-                <Motifs motifs={motifs} sequence={sequence} />
-            )}
-            {DataVerifier.isValidArray(externalCrossReferences) && (
-                <ExternalCrossReferences references={externalCrossReferences} />
-            )}
 
-            {DataVerifier.isValidArray(citations) && (
-                <Citations citations={citations} allCitations={allCitations} />
-            )}
-        </div>
-    )
+  return (
+    <div>
+      {DataVerifier.isValidString(regulonId) ? (
+        <Link to={"/regulon/" + regulonId}>
+          <p style={{ fontSize: "18px" }}>
+            <b dangerouslySetInnerHTML={{ __html: name }} />
+          </p>
+        </Link>
+      ) : (
+        <p style={{ fontSize: "18px" }}>
+          <b dangerouslySetInnerHTML={{ __html: name }} />
+        </p>
+      )}
+
+      <table className="table_auto table_content">
+        <tbody>
+          {DataVerifier.isValidArray(synonyms) && (
+            <tr>
+              <td style={{ fontWeight: "bold" }}>Synonyms: </td>
+              <td>
+                <p dangerouslySetInnerHTML={{ __html: synonyms.join(", ") }} />
+              </td>
+            </tr>
+          )}
+          {DataVerifier.isValidString(sequence) && (
+            <tr>
+              <td style={{ fontWeight: "bold" }}>Sequence:</td>
+              <td>
+                <ViewSequence sequence={sequence} title={`product: ${name}`} />
+              </td>
+            </tr>
+          )}
+          {DataVerifier.isValidString(molecularWeight) && (
+            <tr>
+              <td style={{ fontWeight: "bold" }}>Molecular Weight: </td>
+              <td>{molecularWeight}</td>
+            </tr>
+          )}
+          {DataVerifier.isValidString(anticodon) && (
+            <tr>
+              <td style={{ fontWeight: "bold" }}>Anticodon: </td>
+              <td>{anticodon}</td>
+            </tr>
+          )}
+          {isRegulator && (
+            <tr>
+              <td style={{ fontWeight: "bold" }}>Is regulator?:</td>
+              <td>{isRegulator}</td>
+            </tr>
+          )}
+          {DataVerifier.isValidNumber(isoelectricPoint) && (
+            <tr>
+              <td style={{ fontWeight: "bold" }}>Isoelectric Point: </td>
+              <td>{isoelectricPoint}</td>
+            </tr>
+          )}
+          {DataVerifier.isValidArray(cellularLocations) && (
+            <tr>
+              <td style={{ fontWeight: "bold" }}>Cellular Locations:</td>
+              <td>{cellularLocations.join(", ")}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      {DataVerifier.isValidString(note) && (
+        <Accordion title={<p style={{ fontWeight: "bold" }}>Note</p>}>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: NoteCitations(allCitations, note),
+            }}
+          />
+        </Accordion>
+      )}
+      {DataVerifier.isValidObject(geneOntologyTerms) && (
+        <GOT
+          geneOntologyTerms={geneOntologyTerms}
+          allCitations={allCitations}
+        />
+      )}
+      {DataVerifier.isValidArray(motifs) && (
+        <Motifs motifs={motifs} sequence={sequence} />
+      )}
+      {DataVerifier.isValidArray(externalCrossReferences) && (
+        <ExternalCrossReferences references={externalCrossReferences} />
+      )}
+
+      {DataVerifier.isValidArray(citations) && (
+        <Citations citations={citations} allCitations={allCitations} />
+      )}
+    </div>
+  );
 }
 
-Product.propTypes = PROP_TYPES
-
+Product.propTypes = PROP_TYPES;
 
 /**
  * Description placeholder
@@ -250,15 +261,14 @@ Product.propTypes = PROP_TYPES
  * @returns {React.JSX}
  */
 function Motifs({ motifs = [], sequence }) {
-    return (
-        <Accordion title={<p style={{ fontWeight: "bold" }}>Motifs</p>} >
-            <div>
-                <Motif motifs={motifs} sequence={sequence} />
-            </div>
-        </Accordion>
-    )
+  return (
+    <Accordion title={<p style={{ fontWeight: "bold" }}>Motifs</p>}>
+      <div>
+        <Motif motifs={motifs} sequence={sequence} />
+      </div>
+    </Accordion>
+  );
 }
-
 
 /**
  * Description placeholder
@@ -269,22 +279,20 @@ function Motifs({ motifs = [], sequence }) {
 }
  * @returns {React.JSX}
  */
-function GOT({
-    geneOntologyTerms,
-    allCitations
-}) {
-    return (
-        <Accordion title={<p style={{ fontWeight: "bold" }}>Gene Ontology Terms</p>} >
-            <div>
-                <GeneOntologyTerms
-                    geneOntologyTerms={geneOntologyTerms}
-                    allCitations={allCitations}
-                />
-            </div>
-        </Accordion>
-    )
+function GOT({ geneOntologyTerms, allCitations }) {
+  return (
+    <Accordion
+      title={<p style={{ fontWeight: "bold" }}>Gene Ontology Terms</p>}
+    >
+      <div>
+        <GeneOntologyTerms
+          geneOntologyTerms={geneOntologyTerms}
+          allCitations={allCitations}
+        />
+      </div>
+    </Accordion>
+  );
 }
-
 
 /**
  * Description placeholder
@@ -293,15 +301,14 @@ function GOT({
  * @returns {React.JSX}
  */
 function Citations({ citations, allCitations }) {
-    return (
-        <Accordion title={"Citations"} >
-            <div>
-                <ParagraphCitations citations={citations} allCitations={allCitations} />
-            </div>
-        </Accordion>
-    )
+  return (
+    <Accordion title={"Citations"}>
+      <div>
+        <ParagraphCitations citations={citations} allCitations={allCitations} />
+      </div>
+    </Accordion>
+  );
 }
-
 
 /**
  * Description placeholder
@@ -310,21 +317,21 @@ function Citations({ citations, allCitations }) {
  * @returns {React.JSX}
  */
 function ExternalCrossReferences({ references }) {
-    return (
-        <Accordion title={"External Cross References"} >
-            <div style={{display: "grid", gridTemplateColumns: "25% 25% 25% 25%"}} >
-                {
-                    references.map(((reference) => {
-                        return (
-                            <a style={{ marginLeft: "5px" }} key={"reference_" + reference.externalCrossReferenceId}
-                                href={`${reference.url}`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >{`${reference.externalCrossReferenceName}: ${reference.objectId}`}</a>
-                        )
-                    }))
-                }
-            </div>
-        </Accordion>
-    )
+  return (
+    <Accordion title={"External Cross References"}>
+      <div style={{ display: "grid", gridTemplateColumns: "25% 25% 25% 25%" }}>
+        {references.map((reference) => {
+          return (
+            <a
+              style={{ marginLeft: "5px" }}
+              key={"reference_" + reference.externalCrossReferenceId}
+              href={`${reference.url}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >{`${reference.externalCrossReferenceName}: ${reference.objectId}`}</a>
+          );
+        })}
+      </div>
+    </Accordion>
+  );
 }
