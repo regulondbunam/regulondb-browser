23a1726840c1040afed5ac1cf023a4bd321e0270
Galarcon128 <alarcongabriel128@gmail.com>
2023-10-12 16:14:23 -0600

    add ht builder links

--- src/apps/gene/components/navigation.jsx
+++ src/apps/gene/components/navigation.jsx
@@ -8,22 +8,7 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Collapse from "@mui/material/Collapse";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 600,
-
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function Navigation({
   regulonName,
