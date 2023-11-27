56781569b9e65fd493d3aff9aafcb27c6920023f
Galarcon128 <alarcongabriel128@gmail.com>
2023-11-27 08:58:39 -0600

    implement genomeView

--- src/apps/IGVBrowser/igv.jsx
+++ src/apps/IGVBrowser/igv.jsx
@@ -1,50 +1,49 @@
 import igv from "igv/dist/igv.esm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 let conf = {
-  "id": "Ecoli",
-  "name": "Ecoli",
-  "fastaURL": "/media/raw/e_coli_k12.fna",
-  "indexURL": "/media/raw/e_coli_k12.fna.fai",
-  "order": 1000,
-  "tracks": [
+  id: "Ecoli",
+  name: "Ecoli",
+  fastaURL: "/media/raw/e_coli_k12.fna",
+  indexURL: "/media/raw/e_coli_k12.fna.fai",
+  order: 1000,
+  tracks: [
     {
-      "name": "Genes",
-      "type": "annotation",
-      "url": "/media/raw/gff3/GeneProductSet.gff3",
-      "format": "gff3",
-      "color": "#000000",
-      "displayMode": "EXPANDED",
-    }
-  ]
-}
+      name: "Genes",
+      type: "annotation",
+      url: "/media/raw/gff3/GeneProductSet.gff3",
+      format: "gff3",
+      color: "#000000",
+      displayMode: "EXPANDED",
+    },
+  ],
+};
 
-export default function IGVDraw({igvBrowser, setIgvBrowser}) {
-    const [view, setView] = useState(false)
-    const [genomeLoad, setGenomeLoad] = useState(false)
+export default function IGVDraw({ igvBrowser, setIgvBrowser }) {
+  const [view, setView] = useState(false);
+  const [genomeLoad, setGenomeLoad] = useState(false);
+  useEffect(() => {
     let igvDiv = document.getElementById("igv-divK");
     if (igvDiv && !view) {
-        setView(true)
-        console.log("hola")
-      igv
-        .createBrowser(igvDiv)
-        .then(function (browser) {
-          setIgvBrowser(browser)
-        });
+      setView(true);
+      igv.createBrowser(igvDiv).then(function (browser) {
+        setIgvBrowser(browser);
+      });
     }
-    if(view && igvBrowser){
-      igvBrowser.loadGenome(conf)
+    if (view && igvBrowser && !genomeLoad) {
+      setGenomeLoad(true);
+      igvBrowser.loadGenome(conf);
     }
-    return <>{correct()}</>
-}
+  }, [genomeLoad, view, igvBrowser, setIgvBrowser]);
 
+  return <>{correct()}</>;
+}
 
 function correct() {
   setTimeout(() => {
-      const igv = document.getElementsByClassName("igv-container")
-      if (igv.length>1) {
-          igv[0].remove()
-      }
+    const igv = document.getElementsByClassName("igv-container");
+    if (igv.length > 1) {
+      igv[0].remove();
+    }
   }, 500);
 }
-
