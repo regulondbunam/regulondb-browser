1ad102b24affd4935db8344dc92e86f1dfb9e136
galarcon-ccg <galarcon@ccg.unam.mx>
2024-02-19 15:38:10 -0600

    add draw features

--- src/apps/FeatureMaps/Map/index.jsx
+++ src/apps/FeatureMaps/Map/index.jsx
@@ -7,11 +7,17 @@ import Select from "@mui/material/Select";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 
+const colorFunction = {
+  activator: "green",
+  repressor: "red"
+}
+
 export default function Map({ featureData }) {
   console.log(featureData);
   const { map } = featureData;
   const [distanceTo, setDistanceTo] = useState(map.distanceTo[0]);
   const [zoomLevel, setZoomLevel] = useState(50);
+
 
   const range = Math.round(
     Math.abs(map.trackLeft - map.trackRight) / zoomLevel
@@ -20,11 +26,19 @@ export default function Map({ featureData }) {
   for (let i = 0; i < range+1; i++) {
     const position = map.trackLeft + zoomLevel * (i);
     if (position > 1 && position - zoomLevel < 0) {
-        positions.push("+1")
-    }
-    positions.push(""+position)
+      positions.push({
+        label: "+1",
+        value: 1,
+        width: zoomLevel
+      })
+    }positions.push({
+      label: ""+position,
+      posL: position-zoomLevel,
+      posR: position+zoomLevel,
+      width: zoomLevel*2
+    })
+   
   }
-  console.log(range, zoomLevel);
 
   const handleChange = (event) => {
     setDistanceTo(event.target.value);
@@ -72,12 +86,14 @@ export default function Map({ featureData }) {
       <table className="FM_tableMap" >
         <thead>
           <tr>
-            <th style={{minWidth: "140px"}} >Regulated Entity</th>
-            <th style={{minWidth: "50px"}} >Type</th>
+            <th style={{width: "120px"}} >Regulated Entity</th>
+            <th style={{width: "50px"}} >Type</th>
             {positions.map((position, index) => {
               return (
-                <th key={"position_map_" + position + "_" + index}>
-                  {position}
+                <th key={"position_map_" + position.label + "_" + index}
+                  style={{width: position.width+"px"}}
+                >
+                  {position.label}
                 </th>
               );
             })}
@@ -89,17 +105,8 @@ export default function Map({ featureData }) {
         return (
           <tr
             key={"track_" + key}
-            style={{
-              height: 30,
-            }}
           >
-            <td
-              key={"track_" + key}
-              style={{
-                backgroundColor: "#cadce7",
-                padding: "6px",
-              }}
-            >
+            <td>
               <p
                 style={{
                   fontWeight: "bold",
@@ -118,9 +125,36 @@ export default function Map({ featureData }) {
               />
             </td>
             {positions.map((position, index) => {
+              const features = track.features
+              let _features = []
+              //distanceTo
+              features.forEach(feature => {
+                if (feature.distanceTo[distanceTo]>=position.posL && feature.distanceTo[distanceTo]<=position.posR) {
+                  const rbs = feature.rbs
+                  const size = rbs.rightEndPosition - rbs.leftEndPosition
+                  //console.log(position);
+                  _features.push(
+                    {
+                      position: "relative",
+                      left: Math.abs(Math.abs(position.posL)-Math.abs(feature.distanceTo[distanceTo]))+"px",
+                      width: size+"px",
+                      height: "15px",
+                      backgroundColor: colorFunction[rbs.function]
+                    }
+                  )
+                }
+              });
               return (
-                <td key={"position_track_" + position + "_" + index}>
-                   | 
+                <td key={"position_track_" + position.label + "_" + index} style={{textAlign: "center"}} >
+                  <div className="FM_mapCell">
+                    <div className="FM_mapLine"  />
+                    {_features.map((f,i)=>{
+                      return <div key={"feature"} style={f} className="FM_mapFeature" />
+                    })}
+                    
+                  </div>
+                  
+                 
                 </td>
               );
             })}
