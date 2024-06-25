496c70abe433c6f388cba873aacdd216e80270bd
galarcon-ccg <55556303+galarcon-ccg@users.noreply.github.com>
2024-06-25 10:15:45 -0600

    implement parse json

--- src/apps/ht/datasetPage/info/Info.jsx
+++ src/apps/ht/datasetPage/info/Info.jsx
@@ -17,7 +17,7 @@ export default function Info({datasetId}) {
 
   console.log(_dataset);
   if (_dataset) {
-    const template = "id Dataset: ${_id}\nname: ${sample.title}\n$define PUBLICATION(publication){\ntitle: ${title}\nautors: $[autors].join(',')\n}\npublications: \n$[PUBLICATION(publications)]"
+    const template = "$#comentario\n$#{\ncomentario\nmulti linea\n}#$\nid Dataset: ${_id} $#otro comentario\nname: ${sample.title}\n$define PUBLICATION(publication){\ntitle: ${title}\nautors: ${authors}\n}\npublications: \n$[PUBLICATION(publications)]"
     const parse = new ParseJSONtoTemplate(_dataset,template)
     parse.getCompileText()
   }
--- src/components/transformJSON/index.js
+++ src/components/transformJSON/index.js
@@ -55,12 +55,13 @@ export default class ParseJSONtoTemplate {
             const pathObject = match[2];
 
             // Obtener el objeto del JSON
-            const objectValue = this.getPropertyValue(pathObject.trim());
+            const objectValue = this.getPropertyValueOfObject(pathObject.trim(), this.json);
 
             if (objectValue !== undefined) {
                 // Reemplazar la invocación por el contenido del fragmento
                 const { content } = this.fragments[fragmentName];
-                modifiedTemplate = modifiedTemplate.replace(fullMatch, content);
+                let replacedContent = this.#replaceVariables(content, objectValue);
+                modifiedTemplate = modifiedTemplate.replace(fullMatch, replacedContent);
             } else {
                 // Manejar el caso donde el objeto no está definido
                 console.warn(`Object '${pathObject}' not found in JSON.`);
@@ -85,17 +86,16 @@ export default class ParseJSONtoTemplate {
             const pathArray = match[2];
 
             // Obtener el arreglo del JSON
-            const arrayValue = this.getPropertyValue(pathArray.trim());
+            const arrayValue = this.getPropertyValueOfObject(pathArray.trim(), this.json);
 
             if (Array.isArray(arrayValue)) {
                 // Iterar sobre los elementos del arreglo y reemplazar la invocación por cada elemento
                 let replacement = '';
                 arrayValue.forEach(item => {
                     const { content } = this.fragments[fragmentName];
-                    console.log(content,item);
-                    replacement += this.#replaceVariables(content,item)+'\n'
+                    replacement += this.#replaceVariables(content, item).trim() + '\n';
                 });
-                modifiedTemplate = modifiedTemplate.replace(fullMatch, replacement);
+                modifiedTemplate = modifiedTemplate.replace(fullMatch, replacement.trim());
             } else {
                 // Manejar el caso donde el arreglo no está definido o no es un arreglo
                 console.warn(`Array '${pathArray}' not found in JSON or is not an array.`);
@@ -106,9 +106,9 @@ export default class ParseJSONtoTemplate {
         this.template = modifiedTemplate; // Actualizar el template original con el modificado
     }
 
-    #replaceVariables(template,json) {
+    #replaceVariables(template, json) {
         // Expresión regular para buscar variables en el template
-        const variableRegex = /\${([^}]*)}|\$\[([^\]]*)\]\.join\(\)/g;
+        const variableRegex = /\${([\w.]+)}/g;
 
         let match;
         let modifiedTemplate = template; // Copia del template original para modificaciones
@@ -116,26 +116,19 @@ export default class ParseJSONtoTemplate {
         // Buscar todas las variables en el template
         while ((match = variableRegex.exec(template)) !== null) {
             const fullMatch = match[0];
-            const property = match[1]; // Para ${<propiedad>}
-            const arrayJoin = match[2]; // Para $[array].join()
+            const property = match[1]; // Para ${<propiedad>} y ${<objeto.propiedad>}
 
             if (property !== undefined) {
                 // Reemplazar la variable por el valor correspondiente del objeto JSON
-                const value = this.getPropertyValueOfObject(property.trim(),json);
-                modifiedTemplate = modifiedTemplate.replace(fullMatch, value);
-            } else if (arrayJoin !== undefined) {
-                // Reemplazar el array por su representación unida
-                const arrayValue = this.getPropertyValueOfObject(arrayJoin.trim(),json);
-                if (Array.isArray(arrayValue)) {
-                    modifiedTemplate = modifiedTemplate.replace(fullMatch, arrayValue.join());
-                }
+                const value = this.getPropertyValueOfObject(property.trim(), json);
+                modifiedTemplate = modifiedTemplate.replace(fullMatch, value !== undefined ? value : '');
             }
         }
 
-        return modifiedTemplate; // Actualizar el template original con el modificado
+        return modifiedTemplate; // Devolver el template modificado
     }
 
-    getPropertyValueOfObject(propertyPath,obj) {
+    getPropertyValueOfObject(propertyPath, obj) {
         // Función para obtener el valor de una propiedad en el objeto JSON
         const pathParts = propertyPath.split('.');
         for (const part of pathParts) {
@@ -150,25 +143,22 @@ export default class ParseJSONtoTemplate {
         return obj;
     }
 
-    getPropertyValue(propertyPath) {
-        // Función para obtener el valor de una propiedad en el objeto JSON
-        const pathParts = propertyPath.split('.');
-        let value = this.json;
+    #removeSingleLineComments() {
+        const singleLineCommentRegex = /\$#.*$/gm;
+        this.template = this.template.replace(singleLineCommentRegex, '').trim();
+    }
 
-        for (const part of pathParts) {
-            if (value[part] !== undefined) {
-                value = value[part];
-            } else {
-                // Manejar el caso donde la propiedad no está definida
-                console.warn(`Property '${propertyPath}' not found in JSON.`);
-                return ''; // O devuelve un valor por defecto según tu lógica
-            }
-        }
-
-        return value;
+    #removeMultiLineComments() {
+        const multiLineCommentRegex = /\$#\{[\s\S]*?\}#\$/g;
+        this.template = this.template.replace(multiLineCommentRegex, '').trim();
     }
 
     getCompileText() {
+        // 0 -> eliminar comentarios de una sola línea y multilínea
+        this.#removeMultiLineComments();
+        this.#removeSingleLineComments();
+        
+
         // 1 -> identificar fragmentos y almacenarlos y quitarlos
         this.#fragmentsExtractor();
 
@@ -179,9 +169,9 @@ export default class ParseJSONtoTemplate {
         this.#replaceArrayFragmentInvocations();
 
         // 4 -> reemplazar variables en el template por los valores correspondientes del objeto JSON
-        this.template = this.#replaceVariables(this.template,this.json);
+        this.template = this.#replaceVariables(this.template, this.json);
 
         // Imprimir el template modificado
-        console.log('Modified Template:', this.template);
+        console.log(this.template);
     }
 }
